6a8f31a11ba822864758090ac0c4cfff678d44d4
Chuan Huang <chuhu12@liu.se>
2021-11-15 16:08:30 +0100

    2021-11-15 simulation data has been corrected

--- calc_error.m
+++ calc_error.m
@@ -1,19 +1,18 @@
 function [absPositionError,stdPositionError, absVelError, stdVelError] = calc_error(XData, PData,pos, vel, numSamples, N)
-%UNTITLED 此处显示有关此函数的摘要
-%   此处显示详细说明
+
     absPositionError = zeros(numSamples, N);
     stdPositionError = zeros(numSamples, N);
     absVelError      = zeros(numSamples, N);
     stdVelError      = zeros(numSamples, N);
     for iter = 1 : N
-        absPositionError(:, iter) = abs(XData{iter}(:, 1) - pos(:, 1));
-        stdPositionError(:, iter) = sqrt(PData{iter}(:, 1, 1));
-        absVelError(:, iter) = abs(XData{iter}(:, 2) - vel(:, 1));
-        stdVelError(:, iter) = sqrt(PData{iter}(:, 2, 2));
+        absPositionError(:, iter) = (XData{iter}(:, 1) - pos(:, 1)).^2;
+        stdPositionError(:, iter) = PData{iter}(:, 1, 1);
+        absVelError(:, iter) = (XData{iter}(:, 2) - vel(:, 1)).^2;
+        stdVelError(:, iter) = PData{iter}(:, 2, 2);
     end
-    absPositionError = mean(absPositionError, 2);
-    stdPositionError = mean(stdPositionError, 2);
-    absVelError = mean(absVelError, 2);
-    stdVelError = mean(stdVelError, 2);
+    absPositionError = sqrt(mean(absPositionError, 2));
+    stdPositionError = sqrt(mean(stdPositionError, 2));
+    absVelError = sqrt(mean(absVelError, 2));
+    stdVelError = sqrt(mean(stdVelError, 2));
 end
 
