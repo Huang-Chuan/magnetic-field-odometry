7d59f85e6f07ca669eb31c228be6c2f6d6aaa20e
huangchuan <mrshuangchuan@qq.com>
2021-11-16 20:08:14 +0100

    add acceleration bias

--- calc_error.m
+++ calc_error.m
@@ -1,18 +1,34 @@
-function [absPositionError,stdPositionError, absVelError, stdVelError] = calc_error(XData, PData,pos, vel, numSamples, N)
+function [msePosError,stdPositionError, mseVelError, stdVelError, mseBiasError, stdBiasError] = calc_error(XData, PData, pos,...
+                                                                                                 vel, accbias, ...
+                                                                                                 numSamples, N)
 
-    absPositionError = zeros(numSamples, N);
+    msePosError = zeros(numSamples, N);
     stdPositionError = zeros(numSamples, N);
-    absVelError      = zeros(numSamples, N);
+    mseVelError      = zeros(numSamples, N);
     stdVelError      = zeros(numSamples, N);
+    mseBiasError     = zeros(numSamples, N);
+    stdBiasError     = zeros(numSamples, N);
+    
     for iter = 1 : N
-        absPositionError(:, iter) = (XData{iter}(:, 1) - pos(:, 1)).^2;
+        
+        msePosError(:, iter) = (XData{iter}(:, 1) - pos(:, 1)).^2;
         stdPositionError(:, iter) = PData{iter}(:, 1, 1);
-        absVelError(:, iter) = (XData{iter}(:, 2) - vel(:, 1)).^2;
+        
+        mseVelError(:, iter) = (XData{iter}(:, 2) - vel(:, 1)).^2;
         stdVelError(:, iter) = PData{iter}(:, 2, 2);
+
+        mseBiasError(:, iter) = (XData{iter}(:, 3) - accbias(1, iter)).^2;
+        stdBiasError(:, iter)= PData{iter}(:, 3, 3);
+
     end
-    absPositionError = sqrt(mean(absPositionError, 2));
+
+    msePosError = sqrt(mean(msePosError, 2));
     stdPositionError = sqrt(mean(stdPositionError, 2));
-    absVelError = sqrt(mean(absVelError, 2));
+    
+    mseVelError = sqrt(mean(mseVelError, 2));
     stdVelError = sqrt(mean(stdVelError, 2));
+    
+    mseBiasError = sqrt(mean(mseBiasError, 2));
+    stdBiasError = sqrt(mean(stdBiasError, 2));
 end
 
--- magnetic_model.m
+++ magnetic_model.m
@@ -1,15 +1,24 @@
-function [m] = magnetic_model(x)
+function [m] = magnetic_model(x, type)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     c0 = [0.2; -0.8; 20];
     c1 = [-0.2; -0.1; 20];
     c  = [1; 0; -50];
-    %m = 1e-6 * (c(1) * x^2 + c(2) * x + c(3));
-    %m =  1e-6 * (-0.5 * x + 30);
-    if  x > 0
-        m = 1e-6 * (c0(1) * x^2 + c0(2) * x + c0(3));
-    else
-        m = 1e-6 * (c1(1) * x^2 + c1(2) * x + c1(3));
+    
+    switch type
+        case 0
+            m = 1e-6 * (c(1) * x^2 + c(2) * x + c(3));
+        case 1
+            if  x > 0
+                m = 1e-6 * (c0(1) * x^2 + c0(2) * x + c0(3));
+            else
+                m = 1e-6 * (c1(1) * x^2 + c1(2) * x + c1(3));
+            end
+        case 2
+            m =  1e-6 * (-0.5 * x + 30);
     end
+    
+    
+
 end
 
