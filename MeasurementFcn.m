a07bede37cd3c649bb628e4cfa7643cb3bb47323
Chuan Huang <chuhu12@liu.se>
2021-11-17 15:37:08 +0100

    add magnetometr bias

--- MeasurementFcn.m
+++ MeasurementFcn.m
@@ -2,6 +2,6 @@ function [yk] = MeasurementFcn(x, sensor_disp)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
     r = sensor_disp;
-    yk = 1e-6 * [r^2 -r 1;0 0 1; r^2 r 1] * [x(4);x(5);x(6)];
+    yk = 1e-6 * [r^2 -r 1; 0 0 1; r^2 r 1] * [x(7);x(8);x(9)] + [x(4); x(5); x(6)];
 end
 
